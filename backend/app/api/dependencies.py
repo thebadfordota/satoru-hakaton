ee60ee01d9ac77b705de76cb8e6a0b1af4101bb0
thebadfordota <sasha.skrynnik01@gmail.com>
2024-09-08 14:07:48 +0400

    add file result

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,11 +1,14 @@
 from fastapi import HTTPException
 from fastapi.requests import Request
+from loguru import logger
 
 DEVICE_ID_KEY = 'DEVICE_ID'
 
 
 async def get_device_id(request: Request) -> str:
+    logger.info(f'Headers: {request.headers}')
     device_id = request.headers.get(DEVICE_ID_KEY)
+
     if device_id is None:
         raise HTTPException(status_code=401)
 
